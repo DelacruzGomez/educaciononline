536edc05f56ed95d78d016a1295f93c46244010d
Tutorship <131925129+apiRestJava@users.noreply.github.com>
2026-08-07 17:36:54 -0500

    Actualización final del catálogo de productos y configuración de Vite para producción

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev
 export default defineConfig({
   plugins: [react()],
+  base: '/landing-hotmart/', // 👈 Agregamos la ruta de tu repositorio (según tu package.json)
   server: {
     proxy: {
       // Redirige las peticiones locales del chatbot hacia el servidor local de Netlify

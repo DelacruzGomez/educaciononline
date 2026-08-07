05ffcf9e3606067d1af80def0f26410ea04da8e5
Tutorship <131925129+apiRestJava@users.noreply.github.com>
2026-08-07 17:52:31 -0500

    Actualizacion de catálogo de productos sin base path para Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev
 export default defineConfig({
   plugins: [react()],
-  base: '/landing-hotmart/', // 👈 Agregamos la ruta de tu repositorio (según tu package.json)
   server: {
     proxy: {
-      // Redirige las peticiones locales del chatbot hacia el servidor local de Netlify
       '/.netlify/functions': {
-        target: 'http://localhost:8888', // Puerto por defecto de Netlify Dev
+        target: 'http://localhost:8888',
         changeOrigin: true,
         secure: false,
       }
